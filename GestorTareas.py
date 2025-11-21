1c9ec0be42607fc8eca916bed0f306b09a1e8845
Isaias Cruz <isaiascruzmaister5@gmail.com>
2025-11-21 15:15:01 -0500

    Se agrego el metodo listar tarea y mostrar nombre y estado

--- GestorTareas.py
+++ GestorTareas.py
@@ -1,11 +1,28 @@
 import Tarea
 class GestorTareas:
+
     def __init__(self):
+
         self.tareas=[]
+
     def addTarea(self,nombre,estado):
-        if  nombre=="":
-            print("Ingrese un nombre")
+
+        if  nombre == "":
+
+            print("Ingrese un nombre")  
+
         else:
+
             nuevaTarea=Tarea(nombre,estado)
+
             self.tareas.append(nuevaTarea)
+    def mostarTareas(self):
+        if self.tareas==[]:
+            print("La lista esta vacia")
+        else:
+            i = 0
+            for tarea in self.tareas:
+                print(i++ + ":" + tarea.get_nombre + tarea.get_estado)
+
+
     
