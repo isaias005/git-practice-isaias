236c01341a8335ae7f6d81e7ca27994daf51c256
Isaias Cruz <isaiascruzmaister5@gmail.com>
2025-11-21 14:45:40 -0500

    Creada la clase gestor Tareas

---
+++ Tarea.py
@@ -0,0 +1,15 @@
+class Tarea:
+   nombre=str
+   estado=str
+   def __init__ (self,nombre,estado):
+    self.nombre=nombre
+    self.estado=estado
+
+    def get_nombre(self):
+      return self.nombre
+    def get_estado(self):
+      return self.estado
+    def set_nombre(slef,nomb):
+      self.nombre=nomb
+    def set_estado(slef,est):
+      self.estado=est
