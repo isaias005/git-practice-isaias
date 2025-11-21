aa99ecee90c7dfd11b38c16fab27add390831289
Isaias Cruz <isaiascruzmaister5@gmail.com>
2025-11-21 15:01:20 -0500

    Se agrego el metodo anadir tarea

--- GestorTareas.py
+++ GestorTareas.py
@@ -1,2 +1,11 @@
+import Tarea
 class GestorTareas:
+    def __init__(self):
+        self.tareas=[]
+    def addTarea(self,nombre,estado):
+        if  nombre=="":
+            print("Ingrese un nombre")
+        else:
+            nuevaTarea=Tarea(nombre,estado)
+            self.tareas.append(nuevaTarea)
     
