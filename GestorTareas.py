f310205fa58b75df4d9e81e56397f18c40d8ca46
Isaias Cruz <isaiascruzmaister5@gmail.com>
2025-11-22 17:39:14 -0500

    Metodo de marca como realizado una tarea

--- GestorTareas.py
+++ GestorTareas.py
@@ -16,7 +16,7 @@ class GestorTareas:
             nuevaTarea=Tarea(nombre,estado)
 
             self.tareas.append(nuevaTarea)
-            
+
     def mostarTareas(self):
 
         if self.tareas==[]:
@@ -28,7 +28,33 @@ class GestorTareas:
 
             for tarea in self.tareas:
 
-                print("La tarea" + i++ + ":" + "Su nombre es :" + tarea.get_nombre + "La estado es :" + tarea.get_estado)
+                print("La tarea" + i++ + ":" + "Su nombre es :" + tarea.get_nombre() + "La estado es :" + tarea.get_estado())
+                
+    def marcarTareasPorRealizar(self,pos):
 
+      if pos>len(self.tareas):
+          
+          print(" La posicion esta fuera de rango , ingrese una posicion valida ")
+
+      else:
+          
+          for a in range(pos):
+              
+              if a==pos:
+
+                if(self.tareas[a].get_esatdo()=="Realizado"):
+                      
+                      print(" La tarea q escogio ya estaba realizada favor de escoger otra ")
+
+                elif self.tareas[a].get_estadp() == "Pendiente":
+
+                    self.tareas[a].get_estado() == "Realizado"   
+                             
+        
+
+            
+            
+
+    
 
     
